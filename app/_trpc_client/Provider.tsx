40dea669cfde393d624fd7548d6a9b853b044255
Nitin-Lohumi <lohuminitin@gmail.com>
2025-10-21 21:51:16 +0530

    complete

--- app/_trpc_client/Provider.tsx
+++ app/_trpc_client/Provider.tsx
@@ -4,12 +4,16 @@ import { httpBatchLink } from "@trpc/client";
 import React, { ReactNode, useState } from "react";
 import { trpc } from "./client";
 function Provider({ children }: { children: ReactNode }) {
+  const url =
+    process.env.NODE_ENV === "production"
+      ? "https://blog-app-theta-sage.vercel.app/api/trpc"
+      : "http://localhost:3000/api/trpc";
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url,
         }),
       ],
     })
--- server/trpc/context.ts
+++
@@ -1,5 +0,0 @@
-// import { db } from "@/app/db";
-// export const createContext = () => ({
-//   db,
-// });
-// export type context = ReturnType<typeof createContext>;
